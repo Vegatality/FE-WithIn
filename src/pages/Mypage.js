b4c5d43bfbcb2133d2b8e0e727927280c9ec3737
JungHBae <fruitsforthought@gmail.com>
2023-05-08 11:25:24 +0900

    No change

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -3,8 +3,8 @@ import { FaCamera, FaUser } from "react-icons/fa";
 
 export const Mypage = () => {
   const [editMode, setEditMode] = useState(false);
-  const [name, setName] = useState("John Doe");
-  const [email, setEmail] = useState("johndoe@example.com");
+  const [name, setName] = useState("Some One");
+  const [email, setEmail] = useState("someone@example.com");
   const [image, setImage] = useState("https://via.placeholder.com/150");
 
   const handleNameChange = (event) => {
@@ -64,36 +64,19 @@ export const Mypage = () => {
               <p className="text-md text-gray-500">{name}</p>
             )}
           </div>
-
+          <button
+            className={`${editMode ? "bg-mainPurple" : "bg-mainPurple"} text-white font-bold py-2 px-4 rounded`}
+            onClick={() => setEditMode(!editMode)}
+          >
+            {editMode ? "Cancel" : "Edit"}
+          </button>
           {/* Email */}
           <div className="mb-2">
             <label className="block font-bold text-gray-700 mb-2" htmlFor="email">
               Email
             </label>
-            {editMode ? (
-              <input
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={handleEmailChange}
-                name="email"
-              />
-            ) : (
-              <p className="text-md text-gray-500">{email}</p>
-            )}
-          </div>
 
-          {/* Edit mode toggle button */}
-          <div className="mt-4">
-            <button
-              className={`${
-                editMode ? "bg-mainPurple hover:bg-gray-700" : "bg-mainPurple hover:bg-indigo-700"
-              } text-white font-bold py-2 px-4 rounded`}
-              onClick={() => setEditMode(!editMode)}
-            >
-              {editMode ? "Cancel" : "Edit"}
-            </button>
+            <p className="text-md text-gray-500">{email}</p>
           </div>
         </div>
       </div>
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -14,44 +14,44 @@ import jwtDecode from "jwt-decode";
 import { SET_TOKEN } from "../redux/modules/authSlice";
 
 export const Router = () => {
-    // 페이지 넘어갈 때마다 쿠키가 살아있는지 리덕스에서 꺼내와서 확인한다.
-    // 꺼내왔을 때 없으면 그제서야 이제 서버에 쿠키 요청을 하는거다. (그게 서버와 통신 횟수를 줄여서 훨씬 효율적임!)
-    // 서버에 요청하는 건 리액트 쿼리로 할 예정.
+  // 페이지 넘어갈 때마다 쿠키가 살아있는지 리덕스에서 꺼내와서 확인한다.
+  // 꺼내왔을 때 없으면 그제서야 이제 서버에 쿠키 요청을 하는거다. (그게 서버와 통신 횟수를 줄여서 훨씬 효율적임!)
+  // 서버에 요청하는 건 리액트 쿼리로 할 예정.
 
-    // navigate 는 아무 주소로 처음부터 강제로 접속했을 때는 history가 없기 때문에 원하는 페이지로 보내질 못함.
-    // 그래서 렌더링이 다 끝나는 시점에 실행되는 useEffect 안에 넣어줘야 함.
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const checkCookie = Cookies.get("access");
+  // navigate 는 아무 주소로 처음부터 강제로 접속했을 때는 history가 없기 때문에 원하는 페이지로 보내질 못함.
+  // 그래서 렌더링이 다 끝나는 시점에 실행되는 useEffect 안에 넣어줘야 함.
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const checkCookie = Cookies.get("access");
 
-    // useEffect는 렌더링이 다 끝난 시점에 실행됨.
-    useEffect(() => {
-        if (!checkCookie) {
-            navigate("/login");
-        } else {
-            // 만약 새로고침했을 때 쿠키는 있는데 GlobalState는 초기화되는 현상이 생김.
-            // 그래서 여기서 한 번 더 쿠키를 decode해서 GlobalState로 만들어주는 것.
+  // useEffect는 렌더링이 다 끝난 시점에 실행됨.
+  useEffect(() => {
+    if (!checkCookie) {
+      navigate("/login");
+    } else {
+      // 만약 새로고침했을 때 쿠키는 있는데 GlobalState는 초기화되는 현상이 생김.
+      // 그래서 여기서 한 번 더 쿠키를 decode해서 GlobalState로 만들어주는 것.
 
-            const decodedToken = jwtDecode(checkCookie);
-            const { id } = decodedToken;
-            dispatch(SET_TOKEN({ userName: id }));
-        }
-    }, [navigate]);
+      const decodedToken = jwtDecode(checkCookie);
+      const { id } = decodedToken;
+      dispatch(SET_TOKEN({ userName: id }));
+    }
+  }, [navigate]);
 
-    // useEffect 마운트가 끝난 후에 실행.
+  // useEffect 마운트가 끝난 후에 실행.
 
-    // 렌더링 + 마운트(돔에 추가되는 거)
-    // 재렌더링
+  // 렌더링 + 마운트(돔에 추가되는 거)
+  // 재렌더링
 
-    return (
-        <Layout>
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/mypage" element={<Mypage />} />
-                <Route path="/within/boards/:id" element={<BoardDetails />} />
-            </Routes>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/within/boards/:id" element={<BoardDetails />} />
+      </Routes>
+    </Layout>
+  );
 };
